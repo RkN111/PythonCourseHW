ed268710e1e86f4d3c1b506a55f843b92a65ed91
RkN111 <126549837+RkN111@users.noreply.github.com>
2023-03-16 15:15:40 +0200

    Update Pr4_Task1_odd_or_even.py
    Presentation_4_Odd_or_Even

--- Homework/Pr4_Task1_odd_or_even.py
+++ Homework/Pr4_Task1_odd_or_even.py
@@ -1,5 +1,6 @@
 # Write a program, which will print out for any integer number if it is odd or even.
 
+
 number = int(input('Please enter a number:\n'))
 if number == 0:
     print('Invalid number!')
